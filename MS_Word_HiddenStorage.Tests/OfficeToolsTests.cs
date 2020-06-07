e3560e131c5a033795b958f510b3d5442bff85fc
Alex Butenko <butenko.alexey@gmail.com>
2020-06-07 13:19:10 +0800

    Implemented the core features
    Now it is a simple storage that can write, read and delete a stored
    string by a key.

---
+++ MS_Word_HiddenStorage.Tests/OfficeToolsTests.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Office.Interop.Word;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace OfficeTools.Tests {
+    [TestFixture]
+    public class OfficeToolsTests {
+        [SetUp]
+        public void Setup() {
+            _application = new Application {
+                DisplayAlerts = WdAlertLevel.wdAlertsNone
+            };
+        }
+
+        [TearDown]
+        public void TearDown() {
+            int processId = 0;
+            try {
+                if (_application.Windows.Count > 0) {
+                    int hWnd = _application.Windows[1].Hwnd;
+                    GetWindowThreadProcessId((IntPtr)hWnd, out processId);
+                }
+
+                _application.DisplayAlerts = WdAlertLevel.wdAlertsNone;
+                _application.Quit(WdSaveOptions.wdDoNotSaveChanges);
+            }
+            catch { }
+            finally {
+                try {
+                    Thread.Sleep(3000);
+                    Process.GetProcessById(processId).Kill();
+                }
+                catch { }
+            }
+            try {
+                if (_tmpFile != null && File.Exists(_tmpFile)) File.Delete(_tmpFile);
+            }
+            catch { }
+        }
+
+        [DllImport("user32.dll")]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int processId);
+
+        Application _application;
+        string _tmpFile;
+
+        [Test]
+        public void Ctor_Test_ThrowsNREForNullKey() {
+            Document document = _application.Documents.Add();
+            Assert.Throws<ArgumentNullException>(() => _ = new HiddenStorage(document, null));
+        }
+
+        [Test]
+        public void Ctor_Test_ThrowsArgumentExceptionForEmptyKey() {
+            Document document = _application.Documents.Add();
+            Assert.Throws<ArgumentException>(() => _ = new HiddenStorage(document, " "));
+        }
+
+        [Test]
+        public void Ctor_Test_ThrowsNREForNullDocument() {
+            Assert.Throws<ArgumentNullException>(() => _ = new HiddenStorage(null, "test"));
+        }
+
+        [Test]
+        public void Read_Test_WrongKey_ThrowsKeyNotFoundException() {
+            Document document = _application.Documents.Add();
+            Assert.Throws<KeyNotFoundException>(() => new HiddenStorage(document, "nonExistingKey").Read());
+        }
+
+        [Test]
+        public void Write_Read_Test() {
+            string expected = @"{ test: ""testvalue""}";
+
+            Document document = _application.Documents.Add();
+            string key = "testKey";
+            HiddenStorage storage = new HiddenStorage(document, key);
+
+            storage.Write(expected);
+            string actual = storage.Read();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Write_CloseFile_OpenFile_Read_Test() {
+            string expected = @"{ test: ""testvalue""}";
+            _tmpFile = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".docx");
+
+            Document document = _application.Documents.Add();
+            string key = "testKey";
+            HiddenStorage storage = new HiddenStorage(document, key);
+            storage.Write(expected);
+            document.SaveAs(_tmpFile, WdSaveFormat.wdFormatXMLDocument);
+            document.Close();
+
+            Document documentReopened = _application.Documents.Open(_tmpFile);
+            HiddenStorage storageReopened = new HiddenStorage(documentReopened, key);
+            string actual = storageReopened.Read();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Write_Delete_Read_Test_ThrowsKeyNotFoundException() {
+            string data = @"_";
+
+            Document document = _application.Documents.Add();
+            string key = "testKey";
+            HiddenStorage storage = new HiddenStorage(document, key);
+
+            storage.Write(data);
+            storage.Delete();
+            Assert.Throws<KeyNotFoundException>(() => storage.Read());
+        }
+    }
+}
---
+++ MS_Word_HiddenStorage/HiddenStorage.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Office.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Xml.Linq;
+using Interop = Microsoft.Office.Interop.Word;
+
+namespace OfficeTools {
+    public class HiddenStorage {
+        public HiddenStorage(Interop.Document document, string key) {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            if (string.IsNullOrWhiteSpace(key)) {
+                throw new ArgumentException($"Argument '{key}' cannot be null or white space", nameof(key));
+            }
+            _document = document ?? throw new ArgumentNullException(nameof(document));
+            _key = SecurityElement.Escape(key);
+        }
+
+        readonly string _key;
+        readonly Interop.Document _document;
+        string _id;
+
+        public string Read() {
+            if (!FindAndSetIdByKey()) {
+                throw new KeyNotFoundException();
+            }
+
+            CustomXMLPart xmlPart = _document.CustomXMLParts.SelectByID(_id);
+
+            XElement root = XDocument.Parse(xmlPart.XML).Root;
+
+            return root?.Value == null ? null : FromBase64(root.Value);
+        }
+
+        public void Write(string @object) {
+            XElement root = new XElement(_key) {
+                Value = ToBase64(@object)
+            };
+            string data = root.ToString();
+
+            if (FindAndSetIdByKey()) {
+                _document.CustomXMLParts.SelectByID(_id).Delete();
+            }
+
+            _id = _document.CustomXMLParts.Add(data).Id;
+        }
+
+        public void Delete() {
+            if (!FindAndSetIdByKey()) {
+                throw new KeyNotFoundException();
+            }
+
+            CustomXMLPart xmlPart = _document.CustomXMLParts.SelectByID(_id);
+
+            xmlPart.Delete();
+        }
+
+        bool FindAndSetIdByKey() {
+            _id = _document.CustomXMLParts
+                .Cast<CustomXMLPart>()
+                .SingleOrDefault(p => XDocument.Parse(p.XML).Root?.Name == _key)
+                ?.Id;
+
+            return _id != null;
+        }
+
+        static string FromBase64(string value) =>
+            Encoding.UTF8.GetString(Convert.FromBase64String(value));
+
+        static string ToBase64(string value) =>
+            Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+    }
+}
